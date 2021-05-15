0d5de06052896f304fd7a5c333ac1482fb584009
Alex Busko <alexbusko@icloud.com>
2021-05-15 15:39:20 +0300

    header sm

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,9 +5,17 @@ import { ReactComponent as MerlionLogo } from "../../assets/icons/merlion_logo.s
 import { List } from "react-bootstrap-icons";
 import "./style.scss";
 
+import { Facebook, Instagram } from "react-bootstrap-icons";
+
 const Header = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" fixed="top">
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="primary"
+      variant="dark"
+      fixed="top"
+    >
       <Container>
         <Link to="home" smooth duration={500} offset={-100}>
           <Navbar.Brand>
@@ -21,6 +29,11 @@ const Header = () => {
         />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
+          <Nav.Link>
+              <Link to="whywe" smooth duration={500} offset={-100}>
+                Про нас
+              </Link>
+            </Nav.Link>
             <Nav.Link>
               <Link to="services" smooth duration={500} offset={-100}>
                 Послуги
@@ -31,6 +44,14 @@ const Header = () => {
                 Контакти
               </Link>
             </Nav.Link>
+            <div className="head-social-media">
+              <a href="">
+                <Instagram />
+              </a>
+              <a href="https://www.facebook.com/nazar.kashlyak">
+                <Facebook />
+              </a>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
